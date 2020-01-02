1fc159f987b41d054119b38eca73ec739f302378
PC-REZEK\irezek <irezek@outlook.com>
2020-01-03 00:18:50 +0100

    no message

--- Lab08/script.py
+++ Lab08/script.py
@@ -1,33 +1,3 @@
-# from Cryptodome.PublicKey import ECC
-# from Cryptodome.Hash import SHA256
-# from Cryptodome.Signature import DSS
-#
-# key = ECC.generate(curve='P-256')
-#
-# f = open('myprivatekey.pem','wt')
-# f.write(key.export_key(format='PEM'))
-# f.close()
-#
-# f = open('myprivatekey.pem','rt')
-# key = ECC.import_key(f.read())
-#
-#
-# message = b'I give my permission to order #4355'
-# prvkey = ECC.import_key(open('myprivatekey.pem').read())
-# h = SHA256.new(message)
-# signer = DSS.new(prvkey, 'fips-186-3')
-# signature = signer.sign(h)
-#
-#
-# key = ECC.import_key(open('pubkey.der').read())
-# h = SHA256.new(message)
-# verifier = DSS.new(key, 'fips-186-3')
-# try:
-#     verifier.verify(h, signature)
-#     print("The message is authentic.")
-# except ValueError:
-#     print("The message is not authentic.")
-
 from Cryptodome.PublicKey import DSA
 from Cryptodome.Signature import DSS
 from Cryptodome.Hash import SHA256
